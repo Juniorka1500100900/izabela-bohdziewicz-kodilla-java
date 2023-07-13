7a98818eceb3194089b0637c25d0208fe8431f6e
Izabela Bohdziewicz <izabelabohdziewicz13@wp.pl>
2023-07-13 14:21:01 +0200

    6.2 Calculator test

--- kodilla-testing/src/main/java/com/kodilla/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        Calculator calculator = new Calculator();
+        int addResult = calculator.addAToB(10, 8);
+        int subResult = calculator.subtractBFromA(10, 3);
+        if (addResult == 18 && subResult == 7) {
+            System.out.println("Calculator test OK");
+        } else {
+            System.out.println("Zjebałaś!");
+        }
     }
 }
