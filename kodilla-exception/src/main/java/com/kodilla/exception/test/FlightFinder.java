553adf9325dc20b789fbfd4ba0b8fe42bc8624c9
Izabela Bohdziewicz <izabelabohdziewicz13@wp.pl>
2023-08-02 11:11:51 +0200

    8.4. FlightFinder

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinder.java
@@ -7,13 +7,14 @@ import java.util.Map;
 
 public class FlightFinder {
 
-    public void findFlight(Flight flight) throws RouteNotFoundException {
+    public boolean findFlight(Flight flight) throws RouteNotFoundException {
         Map<String, Boolean> airportMap = new HashMap<>();
         airportMap.put("Madrid", true);
         airportMap.put("Rome", true);
         airportMap.put("Beijing", false);
 
         if (airportMap.containsKey(flight.getArrivalAirport())) {
+            return airportMap.get(flight.getArrivalAirport());
         } else {
             throw new RouteNotFoundException();
         }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightFinderRunner.java
@@ -2,15 +2,16 @@ package com.kodilla.exception.test;
 
 public class FlightFinderRunner {
     public static void main(String[] args) {
-        Flight flight = new Flight("Warsaw", "Rome");
+            Flight flight = new Flight("Warsaw", "Beijing");
         FlightFinder flightFinder = new FlightFinder();
 
         try {
-            flightFinder.findFlight(flight);
+            boolean result = flightFinder.findFlight(flight);
+            System.out.println("Flight available: "+ result);
         } catch (RouteNotFoundException e) {
             System.out.println ("You cannot land in this city!");
         } finally {
-            System.out.println("Have a good flight!");
+            System.out.println("End");
         }
 
     }
