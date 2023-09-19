e6f541c9be58c3fbf1010967853071c62341e157
Izabela Bohdziewicz <izabelabohdziewicz13@wp.pl>
2023-09-19 14:44:18 +0200

    15.5 Bigmac

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,28 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigmacTestSuite {
+    @Test
+    void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("bacon")
+                .ingredient("chef's extra ingredient, you don't want to know")
+                .bun("sesame")
+                .sauce("barbecue")
+                .ingredient("cheese")
+                .ingredient("pickles")
+                .burgers(3)
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        assertEquals(4, howManyIngredients);
+    }
+}
+
+
